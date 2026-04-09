d86fdebede5e141e24e8efbd2f2f8076c36b74bf
GianniRo05 <gianni.jaimes.2005@gmail.com>
2026-04-09 16:02:54 -0400

    Add: create, delete, update, get student endpoints

--- backend/src/core/api.py
+++ backend/src/core/api.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter
+from src.modules.student.routes import router as students_router
 
 router = APIRouter()
+
+router.include_router(students_router, prefix="/api", tags=["Estudiantes"])
