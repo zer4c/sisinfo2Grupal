77051dbc3fad7e09184dc539ac56bac8c74f9d57
GianniRo05 <gianni.jaimes.2005@gmail.com>
2026-04-12 18:32:36 -0400

    feat: add assignmentfile model

---
+++ backend/src/core/files_database.py
@@ -0,0 +1,17 @@
+from fastapi import UploadFile, Response
+
+class FileParser:
+    @staticmethod
+    async def to_bytes(file: UploadFile) -> bytes:
+        file_bytes = await file.read()
+        return file_bytes
+
+    @staticmethod
+    def to_response(file_bytes: bytes, filename: str, media_type: str = "application/octet-stream") -> Response:
+        return Response(
+            content=file_bytes,
+            media_type=media_type,
+            headers={
+                "Content-Disposition": f'attachment; filename="{filename}"'
+            }
+        )
--- backend/src/modules/assignment/routes.py
+++ backend/src/modules/assignment/routes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, File, UploadFile
 from src.core.database import SessionDep
 
 from src.modules.assignment.schemas import AssignmentBase
 from src.modules.assignment.controllers import AssignmentController
+from src.core.files_database import FileParser
 
 router = APIRouter()
 
@@ -26,3 +27,16 @@ async def get_all_assignments_for_subject(session: SessionDep, subject_id: int):
 )
 async def read_assignment(session: SessionDep, assignment_id: int):
     return await AssignmentController.read_assignment(session, assignment_id)
+
+@router.post(
+    "/test-file-conversion",
+    status_code=status.HTTP_200_OK,
+)
+async def test_file_conversion(file: UploadFile = File(...)):
+    file_in_bytes = await FileParser.to_bytes(file)
+    
+    return FileParser.to_response(
+        file_bytes=file_in_bytes, 
+        filename=f"copia_{file.filename}",
+        media_type=file.content_type
+    )
