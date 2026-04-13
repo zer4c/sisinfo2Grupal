dacebed371ee62f09f5c054659945957b8dd55b5
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-04-12 22:10:48 -0400

    fix: add fk in notification model

--- backend/src/modules/assignment/model.py
+++ backend/src/modules/assignment/model.py
@@ -30,24 +30,3 @@ class AssignmentFile(Base):
     data: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
     type_file: Mapped[FileTypeEnum] = mapped_column(Enum(FileTypeEnum), nullable=False)
 
-class SubmissionComment(Base):
-    __tablename__ = "submission_comment"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    submission_id: Mapped[int] = mapped_column(ForeignKey("submission.id"), nullable=False)
-    comment: Mapped[str] = mapped_column(String, nullable=True)
-    created_at: Mapped[date] = mapped_column(Date, nullable=False)
-
-
-class CommentFile(Base):
-    __tablename__ = "comment_file"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    comment_id: Mapped[int] = mapped_column(ForeignKey("submission_comment.id"), nullable=False)
-    data: Mapped[bytes] = mapped_column(LargeBinary, nullable=False)
-    type_file: Mapped[FileTypeEnum] = mapped_column(Enum(FileTypeEnum), nullable=False)
-
-class Notification(Base):
-    __tablename__ = "notification"
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    assignment_id: Mapped[int] = mapped_column(ForeignKey("assignment.id"), nullable=False)
-    student_id: Mapped[int] = mapped_column(ForeignKey("student.id"), nullable=False)
-
