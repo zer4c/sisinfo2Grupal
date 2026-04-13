8a49630a6c8864750433d6008da169cf5148617a
zer4c <pabloazurduy@gmail.com>
2026-04-13 19:03:44 -0400

    feat: add all notificatino bt student

--- backend/src/modules/student/services.py
+++ backend/src/modules/student/services.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from src.core.database import SessionDep
 from src.modules.student.model import Student
 from src.modules.student.schemas import StudentResponse
+from src.modules.comments.model import Notification
 
 
 class StudentService:
@@ -13,5 +14,18 @@ class StudentService:
             if not student_orm:
                 return None
             return StudentResponse.model_validate(student_orm)
+        except Exception:
+            raise
+
+    @staticmethod
+    async def get_all_notification_by_student(session: SessionDep, id: int):
+        try:
+            result = await session.execute(
+                select(Notification).where(Notification.id == id)
+            )
+            result_orm = result.scalars().all()
+            return [
+                Notification.model_validate(notification) for notification in result_orm
+            ]
         except Exception:
             raise
